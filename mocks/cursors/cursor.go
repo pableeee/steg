9162e626a4962595a69d5e521c324168dc5699ef
pableeee <pablo.duco@gmail.com>
2025-12-20 12:43:41 -0300

    updates mocks

--- mocks/cursors/cursor.go
+++ mocks/cursors/cursor.go
@@ -5,89 +5,10 @@
 package mock_cursors
 
 import (
-	image "image"
-	color "image/color"
 	reflect "reflect"
 
 	gomock "github.com/golang/mock/gomock"
 )
-
-// MockChangeableImage is a mock of ChangeableImage interface.
-type MockChangeableImage struct {
-	ctrl     *gomock.Controller
-	recorder *MockChangeableImageMockRecorder
-}
-
-// MockChangeableImageMockRecorder is the mock recorder for MockChangeableImage.
-type MockChangeableImageMockRecorder struct {
-	mock *MockChangeableImage
-}
-
-// NewMockChangeableImage creates a new mock instance.
-func NewMockChangeableImage(ctrl *gomock.Controller) *MockChangeableImage {
-	mock := &MockChangeableImage{ctrl: ctrl}
-	mock.recorder = &MockChangeableImageMockRecorder{mock}
-	return mock
-}
-
-// EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockChangeableImage) EXPECT() *MockChangeableImageMockRecorder {
-	return m.recorder
-}
-
-// At mocks base method.
-func (m *MockChangeableImage) At(x, y int) color.Color {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "At", x, y)
-	ret0, _ := ret[0].(color.Color)
-	return ret0
-}
-
-// At indicates an expected call of At.
-func (mr *MockChangeableImageMockRecorder) At(x, y interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "At", reflect.TypeOf((*MockChangeableImage)(nil).At), x, y)
-}
-
-// Bounds mocks base method.
-func (m *MockChangeableImage) Bounds() image.Rectangle {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Bounds")
-	ret0, _ := ret[0].(image.Rectangle)
-	return ret0
-}
-
-// Bounds indicates an expected call of Bounds.
-func (mr *MockChangeableImageMockRecorder) Bounds() *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Bounds", reflect.TypeOf((*MockChangeableImage)(nil).Bounds))
-}
-
-// ColorModel mocks base method.
-func (m *MockChangeableImage) ColorModel() color.Model {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "ColorModel")
-	ret0, _ := ret[0].(color.Model)
-	return ret0
-}
-
-// ColorModel indicates an expected call of ColorModel.
-func (mr *MockChangeableImageMockRecorder) ColorModel() *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "ColorModel", reflect.TypeOf((*MockChangeableImage)(nil).ColorModel))
-}
-
-// Set mocks base method.
-func (m *MockChangeableImage) Set(x, y int, c color.Color) {
-	m.ctrl.T.Helper()
-	m.ctrl.Call(m, "Set", x, y, c)
-}
-
-// Set indicates an expected call of Set.
-func (mr *MockChangeableImageMockRecorder) Set(x, y, c interface{}) *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Set", reflect.TypeOf((*MockChangeableImage)(nil).Set), x, y, c)
-}
 
 // MockCursor is a mock of Cursor interface.
 type MockCursor struct {
@@ -110,6 +31,20 @@ func NewMockCursor(ctrl *gomock.Controller) *MockCursor {
 // EXPECT returns an object that allows the caller to indicate expected use.
 func (m *MockCursor) EXPECT() *MockCursorMockRecorder {
 	return m.recorder
+}
+
+// Capacity mocks base method.
+func (m *MockCursor) Capacity() int64 {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "Capacity")
+	ret0, _ := ret[0].(int64)
+	return ret0
+}
+
+// Capacity indicates an expected call of Capacity.
+func (mr *MockCursorMockRecorder) Capacity() *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Capacity", reflect.TypeOf((*MockCursor)(nil).Capacity))
 }
 
 // ReadBit mocks base method.
@@ -155,18 +90,4 @@ func (m *MockCursor) WriteBit(bit uint8) (uint, error) {
 func (mr *MockCursorMockRecorder) WriteBit(bit interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
 	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "WriteBit", reflect.TypeOf((*MockCursor)(nil).WriteBit), bit)
-}
-
-// Capacity mocks base method.
-func (m *MockCursor) Capacity() int64 {
-	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Capacity")
-	ret0, _ := ret[0].(int64)
-	return ret0
-}
-
-// Capacity indicates an expected call of Capacity.
-func (mr *MockCursorMockRecorder) Capacity() *gomock.Call {
-	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Capacity", reflect.TypeOf((*MockCursor)(nil).Capacity))
 }
